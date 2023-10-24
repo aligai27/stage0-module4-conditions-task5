b6119eea0b7fd22cd43061d3f8c53729500ebbe3
Aleksei <alekseisligai@gmail.com>
2023-10-24 15:03:49 +0600

    Added switch blocks

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,27 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        switch (year % 4) {
+            default:
+                System.out.println("not leap");
+                break;
+            case 0:
+                switch (year % 100) {
+                    default:
+                        System.out.println("leap");
+                        break;
+                    case 0:
+                        switch (year % 400) {
+                            default:
+                                System.out.println("not leap");
+                                break;
+                            case 0:
+                                System.out.println("leap");
+                                break;
+                        }
+                        break;
+                }
+                break;
+        }
     }
 }
